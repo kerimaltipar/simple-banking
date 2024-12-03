44b8095e558d7a53ec428f1499b7eb88aee3a93d
kerimaltiparmak <kerimaltipar@gmail.com>
2024-12-03 22:45:10 +0300

    bug fix noargsconstructor needed

--- src/main/java/com/eteration/simplebanking/model/DepositTransaction.java
+++ src/main/java/com/eteration/simplebanking/model/DepositTransaction.java
@@ -1,10 +1,13 @@
 package com.eteration.simplebanking.model;
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
 @DiscriminatorValue("DepositTransaction")
+@NoArgsConstructor
 public class DepositTransaction extends Transaction {
 
     public DepositTransaction(double amount, BankAccount bankAccount) {
