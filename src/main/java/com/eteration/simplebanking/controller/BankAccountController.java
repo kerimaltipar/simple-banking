ea6076ec0d6d2ce6511fe378661d5a70ea48a1fc
kerimaltiparmak <kerimaltipar@gmail.com>
2024-12-04 00:12:46 +0300

    endpoints response changed from dto to response entity

--- src/main/java/com/eteration/simplebanking/controller/BankAccountController.java
+++ src/main/java/com/eteration/simplebanking/controller/BankAccountController.java
@@ -5,6 +5,8 @@ import com.eteration.simplebanking.dto.TransactionRequestDTO;
 import com.eteration.simplebanking.dto.TransactionResponseDTO;
 import com.eteration.simplebanking.services.BankAccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,18 +17,18 @@ public class BankAccountController {
     private final BankAccountService bankAccountService;
 
     @GetMapping("v1/{accountNumber}")
-    public BankAccountDTO getAccount(@PathVariable String accountNumber) {
-        return bankAccountService.getBankAccountDTOByNumber(accountNumber);
+    public ResponseEntity<BankAccountDTO> getAccount(@PathVariable String accountNumber) {
+        return new ResponseEntity<>(bankAccountService.getBankAccountDTOByNumber(accountNumber), HttpStatus.OK);
     }
 
     @PostMapping("v1/credit/{accountNumber}")
-    public TransactionResponseDTO credit(@PathVariable String accountNumber, @RequestBody TransactionRequestDTO requestDTO) {
-        return bankAccountService.processDeposit(accountNumber, requestDTO);
+    public ResponseEntity<TransactionResponseDTO> credit(@PathVariable String accountNumber, @RequestBody TransactionRequestDTO requestDTO) {
+        return new ResponseEntity<>(bankAccountService.processDeposit(accountNumber, requestDTO), HttpStatus.OK);
     }
 
     @PostMapping("v1/debit/{accountNumber}")
-    public TransactionResponseDTO debit(@PathVariable String accountNumber, @RequestBody TransactionRequestDTO requestDTO) {
-        return bankAccountService.processWithdrawal(accountNumber, requestDTO);
+    public ResponseEntity<TransactionResponseDTO> debit(@PathVariable String accountNumber, @RequestBody TransactionRequestDTO requestDTO) {
+        return new ResponseEntity<>(bankAccountService.processWithdrawal(accountNumber, requestDTO), HttpStatus.OK);
     }
 
 }
